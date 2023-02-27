992629b9d3573b5636e1f7e23ac1fe88e2c7f389
Kamil Paradowski <kamil.paradowski@yahoo.com>
2023-02-27 15:26:05 +0100

    Rozdział 1: Zadanie 3

---
+++ Rozdział 1/Zadanie 3/Zadanie.java
@@ -0,0 +1,7 @@
+class Zadanie
+{
+  public static void main(String[] args)
+  {
+    System.out.printf("Pierwiastek kwadratowy z liczby π, w zaokrągleniu do dwóch miejsc wynosi: %.2f\n", Math.sqrt(Math.PI));
+  }
+}
