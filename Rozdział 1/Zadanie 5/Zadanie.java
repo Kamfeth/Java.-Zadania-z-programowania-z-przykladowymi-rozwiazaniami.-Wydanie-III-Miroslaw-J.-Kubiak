9f4ea74d66c40d75ac4c6a3f2d3f6afe8bdbe5a7
Kamil Paradowski <kamil.paradowski@yahoo.com>
2023-03-04 20:12:57 +0100

    Rozdział 1: Zadanie 5

---
+++ Rozdział 1/Zadanie 5/Zadanie.java
@@ -0,0 +1,9 @@
+class Zadanie
+{
+  public static void main(String[] args)
+  {
+    final int a = 37;
+    final int b = 11;
+    System.out.printf("Wynik dzielenia całkowitego bez reszty dla liczb %d oraz %d wynosi: %d\n", a, b, a / b);
+  }
+}
