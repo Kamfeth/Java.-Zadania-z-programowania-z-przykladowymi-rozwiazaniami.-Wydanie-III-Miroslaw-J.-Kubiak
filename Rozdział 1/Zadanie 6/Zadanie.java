1fdcd44af9ee85ec7aa44415bedf81033467efba
Kamil Paradowski <kamil.paradowski@yahoo.com>
2023-03-04 23:27:16 +0100

    Rozdział 1: Zadanie 6

---
+++ Rozdział 1/Zadanie 6/Zadanie.java
@@ -0,0 +1,9 @@
+class Zadanie
+{
+  public static void main(String[] args)
+  {
+    final int a = 37;
+    final int b = 11;
+    System.out.printf("Reszta z dzielenia całkowitego dwóch liczb całkowitych %d oraz %d wynosi: %d\n", a, b, a % b);
+  }
+}
